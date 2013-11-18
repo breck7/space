29407b356aaa1e516c66834e1dd591e6f3eb9e9b
Breck <breck7@gmail.com>
2013-11-17 22:48:19 -0800

    Version

--- space.js
+++ space.js
@@ -5,7 +5,7 @@ function Space(content) {
   return this
 }
 
-Space.version = '0.7.3'
+Space.version = '0.7.4'
 
 Space.arrayDelete = function(array, index) {
   return array.slice(0, index).concat(array.slice(index + 1))
