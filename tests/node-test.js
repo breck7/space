f26fd48eb43e168aba7c18293215551c6f457c4b
Breck Yunits <breck@brecks-macbook.local>
2016-03-23 06:26:04 -0700

    Moved test strings to tests file.

--- tests/node-test.js
+++ tests/node-test.js
@@ -1,18 +1,14 @@
 const qunit = require("qunit")
 
 qunit.run({
-	deps: {
-		path: "./tests/testStrings.js",
-		namespace: "testStrings"
-	},
-    code: {
-		// Include the source code
-		path: "./space.js",
-		// What global var should it introduce for your tests?
-		namespace: "Space"
-    },
-    tests: [
-		// Include the test suite(s)
-		"./tests/tests.js"
-    ]
+  code: {
+	 // Include the source code
+	 path: "./space.js",
+	 // What global var should it introduce for your tests?
+	 namespace: "Space"
+  },
+  tests: [
+	 // Include the test suite(s)
+	 "./tests/tests.js"
+  ]
 })
